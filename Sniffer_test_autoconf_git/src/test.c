af7786462865667d46dc26bd12d1531131f939de
BAILLARIN Matthieu <baillarin.matthieu@hotmail.fr>
2015-06-13 17:37:27 +0200

    Ajout Fichier de TEST avec les autoconf et automake

---
+++ Sniffer_test_autoconf_git/src/test.c
@@ -0,0 +1,114 @@
+/*
+    Packet sniffer using libpcap library
+*/
+#include<pcap.h>
+#include<stdio.h>
+#include<stdlib.h> // for exit()
+#include<string.h> //for memset
+ 
+#include<sys/socket.h>
+#include<arpa/inet.h> // for inet_ntoa()
+#include<net/ethernet.h>
+#include<netinet/ip_icmp.h>   //Provides declarations for icmp header
+#include<netinet/udp.h>   //Provides declarations for udp header
+#include<netinet/tcp.h>   //Provides declarations for tcp header
+#include<netinet/ip.h>    //Provides declarations for ip header
+ 
+void process_packet(u_char *, const struct pcap_pkthdr *, const u_char *);
+ 
+FILE *logfile;
+struct sockaddr_in source,dest;
+int tcp=0,udp=0,icmp=0,others=0,igmp=0,total=0,i,j; 
+ 
+int main()
+{
+    pcap_if_t *alldevsp , *device;
+    pcap_t *handle; //Handle of the device that shall be sniffed
+ 
+    char errbuf[100] , *devname , devs[100][100];
+    int count = 1 , n;
+     
+    //First get the list of available devices
+    printf("Finding available devices ... ");
+    if( pcap_findalldevs( &alldevsp , errbuf) )
+    {
+        printf("Error finding devices : %s" , errbuf);
+        exit(1);
+    }
+    printf("Done");
+     
+    //Print the available devices
+    printf("\nAvailable Devices are :\n");
+    for(device = alldevsp ; device != NULL ; device = device->next)
+    {
+        printf("%d. %s - %s\n" , count , device->name , device->description);
+        if(device->name != NULL)
+        {
+            strcpy(devs[count] , device->name);
+        }
+        count++;
+    }
+     
+    //Ask user which device to sniff
+    printf("Enter the number of the device you want to sniff : ");
+    scanf("%d" , &n);
+    devname = devs[n];
+     
+    //Open the device for sniffing
+    printf("Opening device %s for sniffing ... " , devname);
+    handle = pcap_open_live(devname , 65536 , 1 , 0 , errbuf);
+     
+    if (handle == NULL) 
+    {
+        fprintf(stderr, "Couldn't open device %s : %s\n" , devname , errbuf);
+        exit(1);
+    }
+    printf("Device %s in mode listening\n", devname);
+     
+    logfile=fopen("log.txt","w");
+    if(logfile==NULL) 
+    {
+        printf("Unable to create file.");
+    }
+    printf("starting of loging => see results in log.txt");
+     
+    //Put the device in sniff loop
+    pcap_loop(handle , -1 , process_packet , NULL);
+     
+    return 0;   
+}
+ 
+void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *buffer)
+{
+    
+    printf("ONLY DEBUG PURPOSE");
+    int size = header->len;
+     
+    //Get the IP Header part of this packet , excluding the ethernet header
+    struct iphdr *iph = (struct iphdr*)(buffer + sizeof(struct ethhdr));
+    ++total;
+    switch (iph->protocol) //Check the Protocol and do accordingly...
+    {
+        case 1:  //ICMP Protocol
+            ++icmp;
+            break;
+         
+        case 2:  //IGMP Protocol
+            ++igmp;
+            break;
+         
+        case 6:  //TCP Protocol
+            ++tcp;
+            break;
+         
+        case 17: //UDP Protocol
+            ++udp;
+            break;
+         
+        default: //Some Other Protocol like ARP etc.
+            ++others;
+            break;
+    }
+    printf("TCP : %d   UDP : %d   ICMP : %d   IGMP : %d   Others : %d   Total : %d\r", tcp , udp , icmp , igmp , others , total);
+}
+
